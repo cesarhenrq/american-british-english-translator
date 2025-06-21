c61fae6c6c4872a9424a08543bf09f641e730af9
Cesar Henrique <cesaarhvb@gmail.com>
2025-06-20 21:47:07 -0300

    feat(translate): enhance translation display with word highlighting

--- public/index.js
+++ public/index.js
@@ -1,20 +1,19 @@
-
 const translateHandler = async () => {
   const textArea = document.getElementById("text-input");
   const localeArea = document.getElementById("locale-select");
   const errorArea = document.getElementById("error-msg");
   const translatedArea = document.getElementById("translated-sentence");
-  
+
   errorArea.innerText = "";
   translatedArea.innerText = "";
 
   const data = await fetch("/api/translate", {
     method: "POST",
     headers: {
-      "Accept": "application/json",
-      "Content-type": "application/json"
+      Accept: "application/json",
+      "Content-type": "application/json",
     },
-    body: JSON.stringify({"text": textArea.value, "locale": localeArea.value})
+    body: JSON.stringify({ text: textArea.value, locale: localeArea.value }),
   });
 
   const parsed = await data.json();
@@ -23,8 +22,24 @@ const translateHandler = async () => {
     return;
   }
 
-  translatedArea.innerHTML = parsed.translation;
+  const highlighted = highlight(parsed.text, parsed.translation);
+
+  translatedArea.innerHTML = highlighted;
   return;
 };
 
-document.getElementById("translate-btn").addEventListener("click", translateHandler)
+const highlight = (text, translation) => {
+  const textArray = text.split(" ");
+  const translationArray = translation.split(" ");
+  const highlighted = translationArray.map((word, index) => {
+    if (word !== textArray[index]) {
+      return `<span class="highlight">${word}</span>`;
+    }
+    return word;
+  });
+  return highlighted.join(" ");
+};
+
+document
+  .getElementById("translate-btn")
+  .addEventListener("click", translateHandler);
